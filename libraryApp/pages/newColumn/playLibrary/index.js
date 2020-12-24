0f7e471e1ff441e67fffaa04b9e4e111001c462c
wangmingjun <724629707@qq.com>
2020-12-24 16:20:46 +0800

    样式的修改

--- libraryApp/pages/newColumn/playLibrary/index.js
+++ libraryApp/pages/newColumn/playLibrary/index.js
@@ -5,8 +5,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabs:[
+      {
+        id:0,
+        value:"全部记录",
+        isActive:true
+      },
+      {
+        id:1,
+        value:"违约记录",
+        isActive:false
+      }
+      
+    ],
+    activeState:0
   },
+    // 选择
+    handleTabs(e){
+      let {index}=e.detail
+      let {tabs} = this.data
+      tabs.forEach(v => v.id===index?v.isActive=true:v.isActive=false)
+      this.setData({
+        tabs, activeState: index
+      })
+      
+    },
 
   /**
    * 生命周期函数--监听页面加载
